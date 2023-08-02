46287dd04ffaa46fc77430e2b5132b532f7e9c08
juan.zambaldi <juan.zambaldi>
2023-08-02 14:42:32 -0300

    Fiz um menu para o programa para uma melhor interação com o usuário e fiz uma seleção de masculino ou feminino no programa pesoideal.

--- Estudos/ConsoleApp1/Estudos/PesoIdeal.cs
+++ Estudos/ConsoleApp1/Estudos/PesoIdeal.cs
@@ -2,31 +2,40 @@
 {
     public class PesoIdeal
     {
-        const string homem = "H";
-
         public static void Processar()
         {
-            Console.WriteLine("Digite H para homem e M para mulher.");
-            var genero = Console.ReadLine();
+            var opcao = Genero();
+            Processar2(opcao);
+        }
+
+        private static void Processar2(string opcao)
+        {
             Console.WriteLine("Digite sua altura em metros, siga o exemplo: 1,71");
             var altura = Convert.ToDecimal(Console.ReadLine());
 
-            if (EhHomem(genero))
+            switch (opcao)
             {
-                var calculoMasculino = ((72.7M * altura) - 58M);
-                Console.WriteLine($"O seu peso ideal é {calculoMasculino.ToString("F")}");
-            }
-            else
-            {
-                var calculoFeminino = ((62.1M * altura) - 44.7M);
-                //Math.Round(calculoFeminino, 2);
-                Console.WriteLine($"O seu peso ideal é {Math.Round(calculoFeminino, 2)}");
+                case "M":
+                    var calculoMasculino = ((72.7M * altura) - 58M);
+                    Console.WriteLine($"O seu peso ideal é {calculoMasculino.ToString("F")}");
+                    break;
+                case "F":
+                    var calculoFeminino = ((62.1M * altura) - 44.7M);
+                    Math.Round(calculoFeminino, 2);
+                    Console.WriteLine($"O seu peso ideal é {Math.Round(calculoFeminino, 2)}");
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida.");
+                    break;
             }
         }
 
-        private static bool EhHomem(string genero)
+        private static string Genero()
         {
-            return genero == homem;
+            Console.WriteLine("Diga o seu gênero");
+            Console.WriteLine("Digite M para masculino ou F para Feminino.");
+            var opcao = Console.ReadLine();
+            return opcao;
         }
     }
 }
