d0af20cb4ec72b00c4f1ebe6926dc77a15d7ec29
juan.zambaldi <juan.zambaldi>
2023-09-15 18:01:50 -0300

    Coloco o programa ConceitoMedia para a pasta de Media

--- Estudos/ConsoleApp1/Estudos/Media/ConceitoMedia.cs
+++ Estudos/ConsoleApp1/Estudos/Media/ConceitoMedia.cs
@@ -1,4 +1,4 @@
-﻿namespace ConsoleApp1.Estudos
+﻿namespace ConsoleApp1.Estudos.Media
 {
     public class ConceitoMedia
     {
@@ -7,15 +7,14 @@
             Console.Write($"Digite o seu número de identificação. Siga o exemplo: 1234: ");
             var identificacao = Convert.ToInt32(Console.ReadLine());
 
-
             var nota1 = ObterNota("primeira");
             var nota2 = ObterNota("segunda");
             var nota3 = ObterNota("terceira");
-            var mediaExercicios = Math.Round(((nota1 + nota2 + nota3) / 3), 2);
-            var MediaAproveitamento = Math.Round(((nota1 + ((nota2 * 2) + (nota3 * 3) + mediaExercicios)) / 7), 2);
+            var mediaExercicios = Math.Round((nota1 + nota2 + nota3) / 3, 2);
+            var MediaAproveitamento = Math.Round((nota1 + nota2 * 2 + nota3 * 3 + mediaExercicios) / 7, 2);
             var conceito = DeterminaConceito(MediaAproveitamento);
             Console.WriteLine();
-            if ((conceito == "A") || (conceito == "B") || (conceito == "C"))
+            if (conceito == "A" || conceito == "B" || conceito == "C")
             {
                 Console.WriteLine($"Parabéns, você foi aprovado. Sua identificação é {identificacao} Sua média foi de {mediaExercicios}, sua média de aproveitamento foi de {MediaAproveitamento} e seu conceito foi de {conceito}");
             }
@@ -23,15 +22,13 @@
             {
                 Console.WriteLine($"Meus sentimentos, você foi reprovado. Sua identificação é {identificacao} Sua média foi de {mediaExercicios}, sua média de aproveitamento foi de {MediaAproveitamento} e seu conceito foi de {conceito}");
             }
-
-
         }
 
         private static decimal ObterNota(string descreverNota)
         {
             Console.Write($"Digite a sua {descreverNota} nota: ");
             var nota = Convert.ToDecimal(Console.ReadLine());
-            while ((nota < 0) || (nota > 10))
+            while (nota < 0 || nota > 10)
             {
                 Console.Write("Não são aceitas notas maiores que 10 ou menores que 0. Por favor, digite novamente: ");
                 nota = Convert.ToDecimal(Console.ReadLine());
