3da63df4cda8b023e496634dcf550afe7610cbac
juan.zambaldi <juan.zambaldi>
2023-09-27 16:08:16 -0300

    Organizando melhor o ambiente para fazer o Xunit

---
+++ Estudos/PesoIdealText/UnitTest.cs
@@ -0,0 +1,12 @@
+
+namespace PesoIdealText
+{
+    public class UnitTest
+    {
+        [Fact]
+        public void CalculaPeso()
+        {
+            Assert.Equal(61, 49, PesoIdeal.);
+        }
+    }
+}
