57655ce0d268236a5ff37c40b6dd15ec34ecb3bf
juan.zambaldi <juan.zambaldi>
2023-09-29 15:34:59 -0300

    Refatora adicionando a classe dadosIo e adiciona comentários para melhorar o entendimento do programa

--- Estudos/ConsoleApp1/Estudos/Bissexto.cs
+++ Estudos/ConsoleApp1/Estudos/Bissexto.cs
@@ -1,42 +1,34 @@
-﻿using System.ComponentModel.Design;
+﻿using ConsoleApp1.Utils;
+using System.ComponentModel.Design;
 
 namespace ConsoleApp1.Estudos
 {
     public class Bissexto
     {
+        // Exercício: Diga se um ano escrito pelo usuário é bissexto ou não.
         public static void Processar()
         {
+            // Intancia objetos
+            var dadosIo = new DadosIo();
             var anoBissexto = new AnoBissexto();
 
+            //Solicita dado ao usuário
+            anoBissexto.Ano = dadosIo.SolicitarValorInteiroAoUsuario("Digite o ano que deseja saber se é bissexto ou não:", 0);
 
-            anoBissexto.Ano = DigitarAno("Digite o ano que deseja saber se é bissexto ou não.");
-
-
+            // Mostra resultado
             var mensagem = anoBissexto.EhBissexto ? $"O ano {anoBissexto.Ano} é um ano bissexto" : $"O ano {anoBissexto.Ano} não é um ano bissexto";
 
             Console.WriteLine(mensagem);
         }
-
-        public static int DigitarAno(string descricao)
-        {
-            Console.WriteLine(descricao);
-            var ano = Convert.ToInt32(Console.ReadLine());
-            return ano;
-        }
     }
-
-    // Diga se um ano escrito pelo usuário é bissexto ou não.
-
     public class AnoBissexto
     {
-        public int Ano { get; set; }
+        public long Ano { get; set; }
 
         public bool EhBissexto => EhDivisivelPor400 || (EhDivisivelPor4 && !EhDivisivelPor100);
 
         private bool EhDivisivelPor4 => (Ano % 4 == 0);
         private bool EhDivisivelPor100 => (Ano % 100 == 0);
         private bool EhDivisivelPor400 => (Ano % 400 == 0);
-
-
     }
 }
