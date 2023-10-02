7cfe76913798e8f395e4c7d320547db68dd53d1e
juan.zambaldi <juan.zambaldi>
2023-10-02 16:00:24 -0300

    Refatora CalculaIdades com o auxílio de classes, o programa ainda não está finalizado. Estou separando entradas, processamento e saídas. Estou com uma dúvida e irei tirar com o meu chefe.

--- Estudos/ConsoleApp1/Estudos/CalculaIdades.cs
+++ Estudos/ConsoleApp1/Estudos/CalculaIdades.cs
@@ -1,19 +1,25 @@
-﻿namespace ConsoleApp1.Estudos
+﻿using ConsoleApp1.Utils;
+using System.ComponentModel;
+
+namespace ConsoleApp1.Estudos
 {
     public class CalculaIdade
     {
         public static void Processar()
         {
-            var idade1 = ObterIdade("Digite a idade do primeiro homem em anos");
-            var idade2 = ObterIdade("Digite a idade do segundo homem em anos");
-            var idade3 = ObterIdade("Digite a idade da primeira mulher em anos");
-            var idade4 = ObterIdade("Digite a idade da segunda mulher em anos");
-            var homemMaisVelho = (idade1 < idade2) ? idade2 : idade1;
-            var homemMaisNovo = (idade1 < idade2) ? idade1 : idade2;
-            var mulherMaisVelha = (idade3 < idade4) ? idade4 : idade3;
-            var mulherMaisNova = (idade3 < idade4) ? idade3 : idade4;
+            // Inputs
+            var dadosIo = new DadosIo();
+            var comparaIdades = new ComparaIdades();
+            comparaIdades.Idade1 = Convert.ToInt32(dadosIo.SolicitarValorInteiroAoUsuario("Digite a idade do primeiro homem em anos"));
+            comparaIdades.Idade2 = Convert.ToInt32(dadosIo.SolicitarValorInteiroAoUsuario("Digite a idade do segundo homem em anos"));
+            comparaIdades.Idade3 = Convert.ToInt32(dadosIo.SolicitarValorInteiroAoUsuario("Digite a idade do primeira mulher em anos"));
+            comparaIdades.Idade4 = Convert.ToInt32(dadosIo.SolicitarValorInteiroAoUsuario("Digite a idade do segunda mulher em anos"));
 
-            if ((homemMaisNovo == homemMaisVelho) || (mulherMaisNova == mulherMaisVelha))
+            // Process
+
+
+            // Outputs
+            if ((comparaIdades.HomemMaisVelho == comparaIdades.HomemMaisNovo) || (comparaIdades.MulherMaisNova == comparaIdades.MulherMaisVelha))
             {
                 Console.WriteLine();
                 Console.WriteLine("Por favor, digite as idades novamente.As idades dos homens não devem ser iguais, bem como a das mulheres.");
@@ -21,16 +27,41 @@
             else
             {
                 Console.WriteLine();
-                Console.WriteLine($"A soma das idades do homem mais velho com a mulher mais nova é {homemMaisVelho + mulherMaisNova}. " +
-                    $"O produto das idades do homem mais novo com a mulher mais velha é {homemMaisNovo * mulherMaisVelha}.");
-
+                Console.WriteLine(@$"A soma das idades do homem mais velho com a mulher mais nova é {comparaIdades.HomemMaisVelho + comparaIdades.MulherMaisNova}. 
+                    O produto das idades do homem mais novo com a mulher mais velha é {comparaIdades.HomemMaisNovo * comparaIdades.MulherMaisVelha}.");
             }
         }
-        private static int ObterIdade(string descricao)
+
+        public class ComparaIdades
         {
-            Console.WriteLine(descricao);
-            var idade = Convert.ToInt32(Console.ReadLine());
-            return idade;
+            public int Idade1 { get; set; }
+            public int Idade2 { get; set; }
+            public int Idade3 { get; set; }
+            public int Idade4 { get; set; }
+            public int HomemMaisNovo { get; set; }
+            public int HomemMaisVelho { get; set; }
+            public int MulherMaisNova { get; set; }
+            public int MulherMaisVelha { get; set; }
+
+            private List<int> MaisVelho()
+            {
+                var listaIdade = new List<int>();
+
+                while (listaIdade <= 4)
+                {
+                    listaIdade.Add(listaIdade);
+                }
+
+                HomemMaisVelho = (Idade1 < Idade2) ? Idade2 : Idade1;
+
+                HomemMaisNovo = (Idade1 < Idade2) ? Idade1 : Idade2;
+
+                MulherMaisVelha = (Idade3 < Idade4) ? Idade4 : Idade3;
+
+                MulherMaisNova = (Idade3 < Idade4) ? Idade3 : Idade4;
+
+                return listaIdade;
+            }
         }
     }
 }
