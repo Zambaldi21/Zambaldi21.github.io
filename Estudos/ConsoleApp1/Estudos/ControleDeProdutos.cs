274fd28d2696a411d53a0f6731ead4d150de9b3f
juan.zambaldi <juan.zambaldi>
2023-12-01 14:10:40 -0300

    Finaliza o programa, adiciona uma classe auxiliadora, uma função e campos para colaborar com o resultado final.

--- Estudos/ConsoleApp1/Estudos/ControleDeProdutos.cs
+++ Estudos/ConsoleApp1/Estudos/ControleDeProdutos.cs
@@ -8,28 +8,55 @@ namespace ConsoleApp1.Estudos
         {
             //Declara objetos
             var dadosIo = new DadosIo();
+            var produtos = new Produtos();
 
             //Input
-            Console.WriteLine();
+            produtos.KitMotorista = dadosIo.SolicitarValorInteiroAoUsuario("Digite a quantidade de kits entregues aos motoristas: ");
+            produtos.PaoMotorista = dadosIo.SolicitarValorInteiroAoUsuario("Digite a quantidade de pães entregues aos motoristas: ");
+            produtos.CafeMotorista = dadosIo.SolicitarValorInteiroAoUsuario("Digite a quantidade de cafés entregues aos motoristas: ");
+            produtos.AguaMotorista = dadosIo.SolicitarValorInteiroAoUsuario("Digite a quantidade de águas entregues aos motoristas: ");
+            produtos.KitTrabalhadores = dadosIo.SolicitarValorInteiroAoUsuario("Digite a quantidade de kits entregues aos trabalhadores: ");
+            produtos.PaoTrabalhadores = dadosIo.SolicitarValorInteiroAoUsuario("Digite a quantidade de pães entregues aos trabalhadores: ");
+            produtos.CafeTrabalhadores = dadosIo.SolicitarValorInteiroAoUsuario("Digite a quantidade de cafés entregues aos trabalhadores: ");
+            produtos.AguaTrabalhadores = dadosIo.SolicitarValorInteiroAoUsuario("Digite a quantidade de águas entregues aos trabalhadores: ");
+
+            //process
+            var resultadoCafeMoto = produtos.CalcularMotorista(produtos.KitMotorista, produtos.CafeMotorista);
+            var resultadoPaoMoto = produtos.CalcularMotorista(produtos.KitMotorista, produtos.PaoMotorista);
+            var resultadoAguaMoto = produtos.CalcularMotorista(produtos.KitMotorista, produtos.AguaMotorista);
+            var resultadoCafeTrabalhador = produtos.CalcularMotorista(produtos.KitTrabalhadores, produtos.CafeTrabalhadores);
+            var resultadoPaoTrabalhador = produtos.CalcularMotorista(produtos.KitTrabalhadores, produtos.PaoTrabalhadores);
+            var resultadoAguaTrabalhador = produtos.CalcularMotorista(produtos.KitTrabalhadores, produtos.AguaTrabalhadores);
+
+            //Output
+            Console.Clear();
+            Console.WriteLine($"Foram {resultadoCafeMoto} cafés para os motoristas.");
+            Console.WriteLine($"Foram {resultadoPaoMoto} pães para os motoristas.");
+            Console.WriteLine($"Foram {resultadoAguaMoto} águas para os motoristas.");
+            Console.WriteLine($"Foram {resultadoCafeTrabalhador} cafés para os trabalhadores.");
+            Console.WriteLine($"Foram {resultadoPaoTrabalhador} pães para os trabalhadores.");
+            Console.WriteLine($"Foram {resultadoAguaTrabalhador} águas para os trabalhadores.");
+            Console.WriteLine($"Total de cafés foi de {resultadoCafeTrabalhador + resultadoCafeMoto}");
+            Console.WriteLine($"Total de pães foi de {resultadoPaoMoto + resultadoPaoTrabalhador}");
+            Console.WriteLine($"Total de águas foi de {resultadoAguaTrabalhador + resultadoAguaMoto}");
         }
     }
 
-    public class CalculoProdutos
+    public class Produtos
     {
-        public int KitMotorista { get; set; }
-        public int PaoMotorista { get; set; }
-        public int CafeMotorista { get; set; }
-        public int AguaMotorista { get; set; }
-        public int KitTrabalhadores { get; set; }
-        public int PaoTrabalhadores { get; set; }
-        public int CafeTrabalhadores { get; set; }
-        public int AguaTrabalhadores { get; set; }
-        public int Resultado { get; set; }
+        public long KitMotorista { get; set; }
+        public long PaoMotorista { get; set; }
+        public long CafeMotorista { get; set; }
+        public long AguaMotorista { get; set; }
+        public long KitTrabalhadores { get; set; }
+        public long PaoTrabalhadores { get; set; }
+        public long CafeTrabalhadores { get; set; }
+        public long AguaTrabalhadores { get; set; }
+        public long Resultado { get; set; }
 
-        public int CalcularMotorista
-            ()
+        public long CalcularMotorista(long valor1, long valor2)
         {
-
+            Resultado = valor1 + valor2;
             return Resultado;
         }
 
