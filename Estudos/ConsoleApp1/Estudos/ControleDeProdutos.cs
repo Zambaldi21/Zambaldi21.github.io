133ad00c1811f0af9ee56758df173281335dd56c
Zambaldi21 <juanzambaldi12@gmail.com>
2023-11-30 22:16:25 -0300

    Cria programa que irá contabilizar quantos produtos foram utilizadas em determinado dia de distribuição de café, pão e água. Esse programa é para ajudar em um projeto da Igreja batista no Moneró, onde são entregues cafés, águas e pães em um ponto na Ilha do Governador. São entregues para as pessoas que pegam os ônibus e para os motoristas.

---
+++ Estudos/ConsoleApp1/Estudos/ControleDeProdutos.cs
@@ -0,0 +1,38 @@
+﻿using ConsoleApp1.Utils;
+
+namespace ConsoleApp1.Estudos
+{
+    public class ControleDeProdutos
+    {
+        public static void Processar()
+        {
+            //Declara objetos
+            var dadosIo = new DadosIo();
+
+            //Input
+            Console.WriteLine();
+        }
+    }
+
+    public class CalculoProdutos
+    {
+        public int KitMotorista { get; set; }
+        public int PaoMotorista { get; set; }
+        public int CafeMotorista { get; set; }
+        public int AguaMotorista { get; set; }
+        public int KitTrabalhadores { get; set; }
+        public int PaoTrabalhadores { get; set; }
+        public int CafeTrabalhadores { get; set; }
+        public int AguaTrabalhadores { get; set; }
+        public int Resultado { get; set; }
+
+        public int CalcularMotorista
+            ()
+        {
+
+            return Resultado;
+        }
+
+
+    }
+}
