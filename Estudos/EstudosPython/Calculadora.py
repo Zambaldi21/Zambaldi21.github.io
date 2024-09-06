cb20a4be0013ca252d1cd780042a1d4cd4d40d42
Juan Zambaldi <juanzambaldi12@gmail.com>
2024-09-05 21:11:15 -0300

    Faz exercício da calculadora em python aprendendo novos comandos na linguagem

--- Estudos/EstudosPython/Calculadora.py
+++ Estudos/EstudosPython/Calculadora.py
@@ -1,6 +1,22 @@
-numero1 = int(input("Escreva o 1° número: "))
-numero2 = int(input("Escreva o 2° número: "))
+def calculadora(numero_1, numero_2, operador):
+    match operador:
+        
+        case "+":
+            return numero_1 + numero_2
+        
+        case "-":
+            return numero_1 - numero_2
 
-soma = numero1 + numero2
+        case "*":
+            return numero_1 * numero_2
 
-print("A soma é",soma)
+        case "/":
+            return numero_1 / numero_2
+
+numero_1 = float(input("Escreva o 1° número: "))
+numero_2 = float(input("Escreva o 2° número: "))
+operador = input("Escolha um operador entre os 4 sinais, +, -, *, /: ")
+
+resultado = calculadora(numero_1, numero_2, operador)
+
+print("O resultado é:",resultado)
