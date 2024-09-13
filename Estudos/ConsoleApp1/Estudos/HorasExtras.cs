2df2f00f6971b93e74868ef0c27e79d25aa709ba
Juan Zambaldi <juanzambaldi12@gmail.com>
2024-09-13 00:44:41 -0300

    Inicia programa HoraExtra, código apresenta variáveis decisivas, como quantas horas é a sua jornada de trabalho. Cria variáves para receber o horário de entrada e o minuto de entrada e o mesmo para a saída. Converte tudo para hora para realizar o cálculo e no final utiliza o IF para calcular se houve horas extras ou não Esse código está com alguns erros que serão corrigidos ao longo do tempo. Meu visual studio não está rodando nenhum código por problemas de diretório e por isso não consegui realizar nenhum código. Meu primeiro programa que não será usado só por mim e sim para ajudar algumas pessoas do meu trabalho, até mesmo quando eu sair da empresa.

---
+++ Estudos/ConsoleApp1/Estudos/HorasExtras.cs
@@ -0,0 +1,41 @@
+using ConsoleApp1.Utils;
+namespace ConsoleApp1.Estudos
+{
+    public class HorasExtras
+    {
+        public static void Processar()
+        {
+            //inputs
+            var dadosIo = new DadosIo();
+
+            Console.WriteLine("Considere minutos até 59.");
+            var solicitaHorarioColaborador = dadosIo.SolicitarValorInteiroAoUsuario("Digite se você é 4 horas ou 6 horas: ");
+            var solicitaHoraEntrada = dadosIo.SolicitarValorInteiroAoUsuario("Digite a hora que você entrou: ")
+            var solicitaMinutoEntrada = dadosIo.SolicitarValorInteiroAoUsuario("Digite o minuto que você entrou: ", 59)
+            var solicitaHoraSaida = dadosIo.SolicitarValorInteiroAoUsuario("Digite a hora que você saiu: ")
+            var solicitaMinutoSaida = dadosIo.SolicitarValorInteiroAoUsuario("Digite o minuto que você saiu: ", 59)
+
+            //process
+            var converteMinutoHoraEntrada = solicitaMinutoEntrada / 60;
+            var converteMinutoHoraSaida = solicitaMinutoSaida / 60;
+            var calcula = (solicitaHoraEntrada + converteMinutoHoraEntrada) - (solicitaHoraSaida + converteMinutoHoraSaida);
+
+            if (solicitaHorarioColaborador = 4)
+            {
+                var resultado = calcula - 4;
+                return resultado;
+            }
+            else if (solicitaHorarioColaborador = 6)
+            {
+                var resultado = calcula - 6;
+                return resultado;
+            }
+            else if (solicitaHorarioColaborador <= 4 || solicitaHorarioColaborador <= 6)
+            {
+                Console.WriteLine("Você não possui horas extras.")
+            }
+            //output
+            Console.WriteLine("Voce");
+        }
+    }
+}
