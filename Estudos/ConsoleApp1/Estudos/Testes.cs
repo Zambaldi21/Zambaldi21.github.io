aeb01d9a311123618b78339c009bd128ed327adb
Juan Zambaldi <juanzambaldi12@gmail.com>
2024-09-16 22:50:40 -0300

    Continua estudando C# atráves de um curso da UDEMY

--- Estudos/ConsoleApp1/Estudos/Testes.cs
+++ Estudos/ConsoleApp1/Estudos/Testes.cs
@@ -1,42 +1,10 @@
-﻿using ConsoleApp1.Utils;
-
-public class Testes
+﻿namespace ConsoleApp1
 {
-    public static void Processar()
+    public class Testes
     {
-        var dadosIo = new DadosIo();
-
-        //Inputs
-        var valorA = Convert.ToDouble(dadosIo.SolicitarValorDecimalAoUsuario("Digite o valor A: "));
-        var valorB = Convert.ToDouble(dadosIo.SolicitarValorDecimalAoUsuario("Digite o valor B: "));
-        var valorC = Convert.ToDouble(dadosIo.SolicitarValorDecimalAoUsuario("Digite o valor C: "));
-
-        //process
-        var delta = Math.Pow(valorB, 2) - (4 * valorA * valorC);
-        var resultadoPositivo = (-valorB + delta) / (2 * valorA);
-        var resultadoNegativo = (+valorB + delta) / (2 * valorA);
-
-        //Output
-        Console.WriteLine($"Resultado positivo: {resultadoPositivo}");
-        Console.WriteLine($"Resultado negativo: {resultadoNegativo}");
+        public static void Processar()
+        {
+            Console.WriteLine("Hello world");
+        }
     }
 }
-
-/*
-public static int Somar(int n1, int n2)
-{
-    return n1 + n2;
-}
-public static int Subtrair(int n1, int n2)
-{
-    return n1 - n2;
-}
-public static int Multiplicar(int n1, int n2)
-{
-    return n1 * n2;
-}
-public static int Dividir(int n1, int n2)
-{
-    return n1 / n2;
-}
-*/
