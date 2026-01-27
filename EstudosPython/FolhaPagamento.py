54d5959ebd5083e3150bd49c1e7a6e9057915352
Juan Zambaldi <juanzambaldi12@gmail.com>
2026-01-26 23:02:54 -0300

    Cria um else caso o usuário não tenha horas extras facilitando o progama

--- EstudosPython/FolhaPagamento.py
+++ EstudosPython/FolhaPagamento.py
@@ -12,24 +12,27 @@ hora_trabalhada = eh_negativo(hora_trabalhada)
 valor_hora = float(input("Digite o valor da sua hora: "))
 valor_hora = eh_negativo(valor_hora)
 
-salario_bruto = hora_trabalhada * valor_hora
-inss = salario_bruto * 0.08
-salario_liquido = salario_bruto - inss
-
 if hora_trabalhada > 160:
-    diferenca_hora_extra = hora_trabalhada - 160
-    jornada_trabalho = hora_trabalhada - diferenca_hora_extra
-    valor_hora_trabalhada = (hora_trabalhada - diferenca_hora_extra) * valor_hora
+    diferenca_hora_extra = hora_trabalhada - 160  
+    hora_trabalhada = hora_trabalhada - diferenca_hora_extra
+    valor_hora_trabalhada = hora_trabalhada  * valor_hora
     valor_hora_extra = diferenca_hora_extra * (valor_hora + (valor_hora * 0.50))
     salario_bruto = valor_hora_extra + valor_hora_trabalhada
-    inss =  salario_bruto * 0.08    
+    inss = salario_bruto * 0.08
     salario_liquido = salario_bruto - inss
-    print("Seu salário bruto é de:",salario_bruto,". Sendo", jornada_trabalho,"de hora trabalhadas e" ,diferenca_hora_extra,"de horas extras." \
+    
+    print("Olá,", nome,"Seu salário bruto é de:",salario_bruto,". Sendo", hora_trabalhada,"de hora trabalhadas e" ,diferenca_hora_extra,"de horas extras." \
     "\nO valor das sua jornada de trabalho é:", valor_hora_trabalhada,"e o valor da hora extra é:",valor_hora_extra,"." \
     "\nSeu salário líquido é:", salario_liquido,"com o desconto de 8% pelo INSS que foi de:",inss,".")
+else:
+    salario_bruto = hora_trabalhada * valor_hora
+    inss = salario_bruto * 0.08
+    salario_liquido = salario_bruto - inss
 
-print("Seu salário bruto é de:",salario_bruto,". Sendo", hora_trabalhada,"de hora trabalhadas." \
+    print("Olá,", nome,"Seu salário bruto é de:",salario_bruto,". Sendo", hora_trabalhada,"de hora trabalhadas." \
+    "\nO valor das sua jornada de trabalho é:", salario_bruto, \
     "\nSeu salário líquido é:", salario_liquido,"com o desconto de 8% pelo INSS que foi de:",inss,".")
+
 
 """ Crie um programa que: Peça nome, horas trabalhadas e valor da hora, Valide valores negativos Calcule: Salário bruto Hora extra (acima de 160h → +50%) 
 INSS (8%) Mostre um resumo final formatado """
