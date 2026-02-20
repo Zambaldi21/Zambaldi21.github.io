19fcb3e1235ee41d313ad6fdfbd9883c9aef52f2
Juan Zambaldi <juanzambaldi12@gmail.com>
2026-02-20 00:51:45 -0300

    Organiza

--- PrimeiraApi/Controllers/WeatherForecastController.cs
+++ PrimeiraApi/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace PrimeiroApi.Controllers
+namespace PrimeiraApi.Controllers
 {
     [ApiController]
     [Route("[controller]")]
--- PrimeiraApi/WeatherForecast.cs
+++ PrimeiraApi/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace PrimeiroApi
+namespace PrimeiraApi
 {
     public class WeatherForecast
     {
